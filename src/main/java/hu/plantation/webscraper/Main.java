d88f74298f39ce2d427ef5a4f369a024293f56cf
adamszabolcs <38521783+adamszabolcs@users.noreply.github.com>
2020-09-15 23:37:44 +0200

    deleted unnecessary @EnableAutoConfiguration annotation, SpringBootApplication does it already

--- src/main/java/hu/plantation/webscraper/Main.java
+++ src/main/java/hu/plantation/webscraper/Main.java
@@ -2,14 +2,12 @@ package hu.plantation.webscraper;
 
 import hu.plantation.webscraper.config.PropertyConfig;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 @EnableConfigurationProperties(PropertyConfig.class)
 @ComponentScan(basePackages = {"hu.plantation.webscraper"})
 @EnableScheduling
